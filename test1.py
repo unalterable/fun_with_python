86da56ca99f4d1a806b75ded5120ecedcc40687b
Unalterable <diamond.oliver@gmail.com>
2016-07-25 14:08:51 +0100

    modules and lambdas

--- test1.py
+++ test1.py
@@ -1,9 +1,32 @@
 x = 4
 
 print x
+
+print '######1'
+
 def hello():
   if x == 4 : print 'hello'
   print 'inside block'
 print 'outside block'
 
 hello()
+
+print '######2'
+
+import test_module
+
+print '######3'
+
+a = test_module.module_method()
+
+print '######4'
+
+print a #this is nothing as #module_method() returns nothing
+
+print '######5'
+
+a = test_module.module_method
+
+print '######6'
+
+a()
