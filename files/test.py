2cb3e93873c78ea64fd47e8a04fe38ba0bc2a339
Unalterable <diamond.oliver@gmail.com>
2016-07-27 11:32:17 +0100

    fun with files

--- files/test.py
+++ files/test.py
@@ -1,6 +1,37 @@
+testfile1 = 'test1.txt'
+testtext1 = 'hello testfile1'
+
+
 # creating a test file
-f1 = open('test1.txt', 'w')
-f1.write('hello test1.txt')
+print '1: try to create file'
+f1 = open(testfile1, 'w')
+f1.write(testtext1)
+print '1: done'
+print
 
 
+# try to read the file without closing
+print '2: read and print file contents (without closing)'
+f2 = open(testfile1, 'r')
+print f2.read()
+print '2: done'
+print
 
+
+# close and try to read again
+print '3: close and try again'
+f1.close()
+f2.close()
+
+f3 = open(testfile1, 'r')
+print f3.read()
+print '3: done'
+print
+
+# delete file
+print '4: delete file'
+import os
+os.remove(testfile1)
+print "File Removed!"
+print 'done'
+print
